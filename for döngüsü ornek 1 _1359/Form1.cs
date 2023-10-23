8368f13059f2811905f3d88650a4d7b45fb3cdc1
rabiaakilic <147382992+rabiaakilic@users.noreply.github.com>
2023-10-23 12:22:45 +0300

    Add files via upload

---
+++ for döngüsü ornek 1 _1359/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_döngüsü_ornek_1__1359
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string ad =txtAd.Text;
+
+            for(int i = 0; i <10; i++)  
+            {
+                string metin = i.ToString() + "❤️" + ad;
+                lbAd.Items.Add(metin);
+            }
+
+        }
+    }
+}
